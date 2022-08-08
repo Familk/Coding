ae37e9bdbaa7e5d80547055e8a303a1c86cd7640
Familk <congthuongnho@gmail.com>
2022-08-08 21:46:24 +0700

    Update C Sharp 2

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -1,1 +1,11 @@
-using 
+using System;
+
+namespace HelloWorld 
+{
+    class Program 
+    {
+        staric void Main(string[] args) {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
